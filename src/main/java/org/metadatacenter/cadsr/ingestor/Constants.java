98feae7abb96ebc7891410ba611cd8f3b9c828b4
Marcos Martinez <marcos.martinez.romero@gmail.com>
2018-07-10 18:35:54 -0700

    Initial development to extract large value sets as an ontology

---
+++ src/main/java/org/metadatacenter/cadsr/ingestor/Constants.java
@@ -0,0 +1,10 @@
+package org.metadatacenter.cadsr.ingestor;
+
+public final class Constants {
+  // Minimum number of values in a value set for it to be defined as a BioPortal value set
+  public static final int MIN_BP_VALUESET_SIZE = 20;
+
+  public static final String CDE_VALUESETS_ONTOLOGY_IRI = "https://schema.metadatacenter.org/ontologies/CDESVS";
+  public static final String CDE_VALUESETS_ONTOLOGY_NAME = "cdesvs.owl";
+  public static final String CDE_STATUS_RELEASED = "RELEASED";
+}
